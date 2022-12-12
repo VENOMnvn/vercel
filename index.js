b3c7eb6993561c81c8947916935eb6e64f197504
Naveen Chaudhary <98143534+VENOMnvn@users.noreply.github.com>
2022-12-12 12:55:41 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,100 @@
+const express = require('express');
+const app = express();
+
+var bodyParser = require('body-parser'); 
+
+const getDataUser = require('./GetUserData');
+const AddNewUser = require('./AddNewUser');
+const SearchUser = require('./SearchUser');
+const cors = require('cors');
+const path = require('path');
+app.use(cors());
+app.use(bodyParser.json());
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname,'public')));
+
+app.get('/',(rq,rs)=>{
+   rs.send("naveen");
+});
+
+
+
+app.post("/signup",async (req,res)=>{
+   console.log(req.body.email);
+   const result = await SearchUser({"email":req.body.email});
+   if(!result.found)
+   {
+      const response =  await AddNewUser(req.body);
+      res.send({response,result,"EmailValid":true});
+   }
+   else
+   {
+      res.send({"EmailValid":false})
+   }
+})
+
+app.post('/login',async (req , res)=>{
+   const result = await SearchUser({"email":req.body.email});
+   console.log(result);
+   let auth =(result.response[0].password==req.body.password);
+   if(auth)
+   {
+      res.send({"valid":auth,"User":result})
+   }
+   else
+   {
+      res.send({"valid":auth});
+   }
+})
+
+
+
+
+const http = require('http').Server(app);
+
+const PORT = 4000 || process.env.PORT ;
+
+const socketIO = require('socket.io')(http, {
+    cors: {
+        origin: "*"
+          }
+});
+
+app.use(cors());
+
+let ActiveUser=[];
+
+socketIO.on('connection',(socket)=>{
+   console.log("Server ID");
+   
+   socket.on('send',(data)=>{
+      console.log(data);
+      socketIO.emit('response',data);
+   });
+
+   socket.on('userData',(data)=>{
+
+     if(!ActiveUser.includes(data)) 
+     ActiveUser.push(data);
+
+     socketIO.emit("responseActiveUser",ActiveUser);
+   })
+    
+   socket.on('typing',(data)=>{
+     console.log(data);
+     socketIO.emit('Typingresponse',data);
+   });
+});
+
+
+if(process.env.NODE_ENV=='production'){
+   app.get('/',(req,res)=>{
+       app.use(express.static(path.resolve(__dirname,'public')))
+       res.sendFile(path.resolve(__dirname,'public','index.html'))
+   })
+}
+  
+http.listen(PORT, () => {
+   console.log(`Server listening on ${PORT}`);
+},"0.0.0.0");
